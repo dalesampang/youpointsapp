278a8ab16468c9d4787928455c05f55277ad6d02
Christian Dale Sampang <cdalesampang@outlook.com>
2018-01-17 19:01:07 +0800

    updated index

--- www/js/index.js
+++ www/js/index.js
@@ -37,6 +37,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        window.location="https://my.alifelong.biz";
+        // window.location="https://my.alifelong.biz";
+        window.open('https://my.alifelong.biz', '_self ', 'location=yes');
     }
 };
